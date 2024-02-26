59cf4766f82aea6c20c1e22f6c01e1111e1ad25e
“SUNDAR-Office” <aaranoffice@gmail.com>
2024-02-26 11:32:18 +0530

    #5 Create City

---
+++ resources/views/livewire/dashboard/index.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <x-slot name="header">Dashboard</x-slot>
+
+    <x-forms.m-panel>
+
+        Success is as dangerous as failure.
+
+    </x-forms.m-panel>
+
+</div>
---
+++ resources/views/livewire/webs/home/index.blade.php
@@ -0,0 +1,3 @@
+<div class="p-5">
+     Close your eyes. Count to one. That is how long forever feels.
+</div>
