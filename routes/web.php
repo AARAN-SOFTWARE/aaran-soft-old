01a2fc242bd08ac36b1287851c52b3d493e44e54
kalai <kalaiararasan200229@gmail.com>
2024-02-26 14:20:51 +0530

    #9 working on category, size, colour, state

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',
 //Common
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
 
-    Route::get('/cities', App\Livewire\Common\CityList::class)->name('cities');
+    Route::get('cities', App\Livewire\Common\CityList::class)->name('cities');
+    Route::get('states', App\Livewire\Common\StateList::class)->name('states');
+
+    Route::get('categories', App\Livewire\Common\CategoryList::class)->name('categories');
+
+    Route::get('colours', App\Livewire\Common\ColourList::class)->name('colours');
+    Route::get('sizes', App\Livewire\Common\SizeList::class)->name('sizes');
 
 });
