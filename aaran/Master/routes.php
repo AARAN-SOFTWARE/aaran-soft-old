c72531894284c763a26a4c8d2c28ad0821349955
SUNDAR <aaransoftware@gmail.com>
2024-02-28 08:23:37 +0530

    #14 rework on route

--- aaran/Master/routes.php
+++ aaran/Master/routes.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 //master
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::get('products', App\Livewire\Master\Product\Index::class)->name('products');
+
+    Route::get('/contacts', App\Livewire\Master\Product\Index::class)->name('contacts');
+    Route::get('/products', App\Livewire\Master\Product\Index::class)->name('products');
+    Route::get('/companies', App\Livewire\Master\Product\Index::class)->name('companies');
+
 });
