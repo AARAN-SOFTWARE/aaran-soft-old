017fd4a8229b8c4a893dca8f4b819446614f899e
“SUNDAR-Office” <aaranoffice@gmail.com>
2024-02-28 20:32:26 +0530

    #23 refactor test

--- aaran/Common/Tests/CityTest.php
+++ aaran/Common/Tests/CityTest.php
@@ -3,6 +3,7 @@
 namespace Aaran\Common\Tests;
 
 use Aaran\Common\Models\City;
+use App\Livewire\Common\CityList;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -16,7 +17,16 @@ class CityTest extends TestCase
 
         $this->assertEquals('City', actual: $city->vname);
         $this->assertEquals('1', actual: $city->active_id);
+    }
 
+    public function test_Create()
+    {
+        \Livewire::test(CityList::class)
+            ->set(['vname' => 'sundar city', 'active_id' => '1'])
+            ->call('save');
 
+        $obj = City::find(1);
+
+        $this->assertEquals('Sundar city', $obj->vname);
     }
 }
