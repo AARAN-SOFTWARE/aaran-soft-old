9d9d094b8eb4b621119e95265cc394ceea051eeb
Gowtham <gowtham@aaran.org>
2024-02-28 12:15:03 +0530

    #18 work on upsert route

--- aaran/Master/routes.php
+++ aaran/Master/routes.php
@@ -7,6 +7,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
     Route::get('/contacts', App\Livewire\Master\Product\Index::class)->name('contacts');
     Route::get('/products', App\Livewire\Master\Product\Index::class)->name('products');
+    Route::get('products/{id}/upsert', App\Livewire\Master\Product\Upsert::class)->name('products.upsert');
     Route::get('/companies', App\Livewire\Master\Product\Index::class)->name('companies');
 
 });
