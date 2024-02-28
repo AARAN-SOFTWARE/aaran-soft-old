66a6c368a43cf735d4a2da1dac599469bfe43ea5
Gowtham <gowtham@aaran.org>
2024-02-28 11:57:36 +0530

    #14 work on advanced-search and per-page

--- resources/views/components/forms/per-page.blade.php
+++ resources/views/components/forms/per-page.blade.php
@@ -1,6 +1,6 @@
 <x-input.group borderless paddingless for="perPage" label="Per Page">
     <label for="perPage">&nbsp;</label>
-    <select wire:model="perPage" wire:change.debounce="reRender" id="perPage" class="w-20 purple-textbox" >
+    <select wire:model.live="perPage" id="perPage" class="w-20 purple-textbox" >
         <option value="10">10</option>
         <option value="25">25</option>
         <option value="50">50</option>
