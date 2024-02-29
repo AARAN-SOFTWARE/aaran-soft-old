78989f04710dbde8a9fe0902e26404c2c0fdbeda
Divya <divya@aaran.org>
2024-02-29 12:02:58 +0530

    #25 company table

---
+++ database/migrations/2024_02_29_062525_create_companies_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+            Schema::create('companies', function (Blueprint $table) {
+                $table->id();
+                $table->string('vname');
+                $table->string('display_name')->nullable();
+                $table->string('address_1')->nullable();
+                $table->string('address_2')->nullable();
+                $table->foreignId('city_id')->references('id')->on('cities');
+                $table->foreignId('state_id')->references('id')->on('states');
+                $table->foreignId('pincode_id')->references('id')->on('pincodes');
+                $table->string('mobile')->nullable();
+                $table->string('whatsapp')->nullable();
+                $table->string('landline')->nullable();
+                $table->string('gstin')->nullable();
+                $table->string('pan')->nullable();
+                $table->string('email')->nullable();
+                $table->string('website')->nullable();
+                $table->string('active_id', 3)->nullable();
+                $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
