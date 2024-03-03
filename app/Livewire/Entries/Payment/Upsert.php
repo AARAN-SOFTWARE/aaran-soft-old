ffaf2b98e79123abe125bb4873c1537c7e7e628f
SUNDAR <aaransoftware@gmail.com>
2024-03-03 09:48:07 +0530

    #40 added livewire components for purchase , payment , receipt

---
+++ app/Livewire/Entries/Payment/Upsert.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Entries\Payment;
+
+use Livewire\Component;
+
+class Upsert extends Component
+{
+    public function render()
+    {
+        return view('livewire.entries.payment.upsert');
+    }
+}
