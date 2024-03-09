387e249bf15f111a85e5123fadb529c307326983
Gowtham <gowtham@aaran.org>
2024-03-09 19:25:22 +0530

    #65 sales gst work and loading window page on city list

--- app/Livewire/Entries/Sales/Upsert.php
+++ app/Livewire/Entries/Sales/Upsert.php
@@ -726,9 +726,11 @@ class Upsert extends Component
         if ($this->itemList) {
             $this->total_qty = 0;
             $this->round_off=0;
+            $this->total_gst=0;
             foreach ($this->itemList as $row) {
                 $this->total_qty += round(floatval($row['qty']),3);
                 $this->round_off += round(floatval($row['price'])*$row['qty'], );
+                $this->total_gst = round(floatval($row['gst_percent']),);
             }
         }
     }
